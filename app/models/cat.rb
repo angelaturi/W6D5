346a13243e75506ccd6dbb238d7ef9fe6e2ab7bb
aturi <angelaturi99@gmail.com>
2021-04-02 14:45:28 -0400

    Start phase I

---
+++ app/models/cat.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :sex, inclusion: { in: %w(M F) }
+    
+end
